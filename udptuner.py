8efdc2dc78a202cd60e9eb0c9e93a42ea39fcfed
rh1n0din0 <ediaz790@gmail.com>
2017-10-12 13:36:22 -0400

    updated basic functionality and classes

--- udptuner.py
+++ udptuner.py
@@ -5,10 +5,26 @@ import sys
 from threading import Thread
 from queue import Queue
 
+    """
+    UDPTuner is a tool which allows one to test the flow of UDP packets
+    throughout a network. The tool works by sending a variety of network
+    packets which are then used for analysis.
+    -- Eli Diaz
+    """
+
 def logger(msg):
-    sys.stdout.write('\n - - - - - - - - - - - - \n')
+    sys.stdout.write('\n' + ' -' * 30 + '\n')
+    sys.stdout.write('\t')
     sys.stdout.write(str(msg))
-    sys.stdout.write('\n - - - - - - - - - - - - \n')
+    sys.stdout.write('\n' + ' -' * 30 + '\n')
+
+def payload_gen(size, pkt_size, **kwargs):
+    payload = []
+    for packet in range(size):
+        p = (str(packet) + ':').encode('UTF-8')  # Use num as packet header
+        p = p + (b'0' * pkt_size)[:pkt_size]  # pad with 0's
+        payload.append(p)
+    return payload
 
 class udp_sender:
 
@@ -25,7 +41,7 @@ class udp_sender:
     def send_payload(self):
         for packet in self.payload:
             if type(packet) != bytes:
-                packet = packet.encode('UTF-8')
+                packet = str(packet).encode('UTF-8')
             self._send(packet)
 
     def completed(self):
@@ -58,6 +74,7 @@ class sync_server(Thread):
         self._listen()
 
     def terminate(self):
+        logger('Terminating . . . ')
         self.socket.close()
         exit()
 
@@ -82,34 +99,56 @@ class sync_server(Thread):
         self.socket.listen(1)
         while True:
             try:
-                s, client_addr = self.socket.accept()
-                self._handle_client(s, client_addr)
+                self.client_sock, self.client_addr = self.socket.accept()
+                self._handle_client()
             except KeyboardInterrupt:
+                logger('Received CTRL+C')
                 self.terminate()
             except socket.error as e:
                 logger(e)
 
-    def _handle_client(self, s, addr):
+    def _handle_client(self):
+        addr = self.client_addr
         logger('Received connection from %s on port %s' % (addr[0], addr[1]))
-        self._handshake(s, addr)
+        self._handshake()
 
-    def _handshake(self, sock, addr):
+    def _handshake(self):
         logger('Syncing with client . . .')
-        sock.send(self.handshake_msg)
-        data = sock.recv(512)
+        self.client_sock.send(self.handshake_msg)
+        data = self.client_sock.recv(512)
         if data and data == self.handshake_msg:
-            self.sync_time(sock)
+            self.sync_time()
         else:
             logger('Received invalid handshake!')
             logger('Waiting for new connection . . .')
-            sock.close()
+            self.client_sock.close()
             self._listen()
 
-    def sync_time(self, sock):
+    def _start_test(self):
+        payload = payload_gen(60000, 1024)
+        host, _ = self.client_addr
+        self.sender = udp_sender(host, 30000, payload)
+        logger('Creating client payload . . .')
+        payload_data = str(len(payload)) + ':' + '30000'
+        logger('Sending client metadata . . .')
+        self.client_sock.send(payload_data.encode('UTF-8'))
+        logger('Waiting for client . . .')
+        data = self.client_sock.recv(512)
+        if data and data == b'UDPTunerClientREADY!':
+            logger('Sending UDP payload . . .')
+            self.sender.send_payload()
+            self.sender.payload = [b'@******#']
+            self.sender.send_payload()
+        logger('Done, waiting for client report . . .')
+        data = self.client_sock.recv(512)
+        logger(data.decode())
+        self.terminate()
+
+    def sync_time(self):
         logger('Syncing time . . .')
         stamps = ':'.join([str(time.time()) for x in range(30)])
-        sock.send(stamps.encode('UTF-8'))
-        data = sock.recv(1024).decode()
+        self.client_sock.send(stamps.encode('UTF-8'))
+        data = self.client_sock.recv(1024).decode()
         stamps = stamps.split(':')
         client_stamps = data.split(':')
         if len(client_stamps) == 30:
@@ -117,7 +156,8 @@ class sync_server(Thread):
             client_stamps = [float(x) for x in client_stamps]
             deltas = [x - y for x in stamps for y in client_stamps]
             self.time_delta = sum(deltas)/float(len(deltas))
-            logger('Time Delta: ' + str(round(self.time_delta, 4)))
+            logger('Time Delta: ' + str(round(self.time_delta, 4)) + 'ms')
+            self._start_test()
 
 class sync_client(sync_server):
 
@@ -150,7 +190,39 @@ class sync_client(sync_server):
             client_stamps = [float(x) for x in client_stamps]
             deltas = [x - y for x in stamps for y in client_stamps]
             self.time_delta = sum(deltas)/float(len(deltas))
-            logger('Time Delta: ' + str(round(self.time_delta, 4)))
+            logger('Time Delta: ' + str(round(self.time_delta, 4)) + 'ms')
+            self._start_test()
+
+    def _start_test(self):
+        logger('Waiting for server . . .')
+        data = self.socket.recv(512)
+        pkts, recv_port = data.decode('UTF-8').split(':', 2)
+        self.udp_sock = socket.socket(self.socket_type, socket.SOCK_DGRAM)
+        logger('Binding UDP socket on port %s' % recv_port)
+        try:
+            self.udp_sock.bind(('', int(recv_port)))
+            self.udp_sock.settimeout(5.0)
+        except socket.error as e:
+            logger(e)
+        payload = b''
+        logger('Ready to receive UDP data . . .')
+        self.socket.send(b'UDPTunerClientREADY!')
+        while True:
+            try:
+                data = self.udp_sock.recv(4096)
+            except socket.timeout:
+                logger('UDP socket timed out')
+                break
+            payload += data
+            if len(data) == 0:
+                break
+        logger('Data tranfer complete, now terminating . . .')
+        data_len = str(len(payload))
+        self.socket.send(('Client received %s bytes of data' %
+                         data_len).encode())
+
+        time.sleep(10)
+        self.terminate()
 
     def _connect(self):
         try:
@@ -168,3 +240,11 @@ class sync_client(sync_server):
             logger('Received invalid handshake!')
             logger('Terminating . . .')
             self.terminate()
+
+if sys.argv[1] == 'server':
+    server = sync_server()
+    server.start()
+elif sys.argv[1] == 'client':
+    client = sync_client(sys.argv[2], int(sys.argv[3]))
+    client.start()
+
